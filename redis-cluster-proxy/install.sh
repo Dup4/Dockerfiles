3294d0964f0201d4699f83bc790f9edd053498f9
Dup4 <lyuzhi.pan@gmail.com>
2022-10-10 20:16:53 +0800

    feat: redis-cluster-proxy

---
+++ redis-cluster-proxy/install.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+TOP_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+
+cd "${TOP_DIR}" || exit 1
+
+REDIS_CLUSTER_PROXY="redis-cluster-proxy"
+
+git clone https://github.com/RedisLabs/redis-cluster-proxy.git "${REDIS_CLUSTER_PROXY}"
+
+cd "${TOP_DIR}/${REDIS_CLUSTER_PROXY}" || exit 1
+
+make
+make install
